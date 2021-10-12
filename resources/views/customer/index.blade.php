bc0f1ce6736dc53ea2a639156a7720cb1dde344d
martogasha <m.gachanga97@gmail.com>
2021-10-12 17:39:50 +0300

    sfjkkjgrdfgs

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -365,7 +365,21 @@
     <script src="js/custom.js"></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-EG6TPLN1NJ"></script>
     </body>
+    <!-- Facebook Pixel Code -->
+
+
+    <!-- End Facebook Pixel Code -->
     <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '966152563941099');
+        fbq('track', 'PageView');
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -441,6 +455,9 @@
         })
 
     </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=966152563941099&ev=PageView&noscript=1"
+        /></noscript>
 
     <!-- Mirrored from html.lionode.com/fastro/f001/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 May 2020 05:21:23 GMT -->
     </html>
