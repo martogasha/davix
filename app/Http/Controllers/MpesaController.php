46856b6a10afb55249472320b48bd30b4b1d7651
martogasha <miringupk3@gmail.com>
2021-10-29 14:23:02 +0300

    sdrsasjhdh

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -24,7 +24,7 @@ class MpesaController extends Controller
         $stk = $K2->StkService();
         $result = $stk->initiateIncomingPayment([
             'paymentChannel' => 'M-PESA STK Push',
-            'tillNumber' => '000798',
+            'till_number' => '000798',
             'firstName' => 'Jane',
             'lastName' => 'Doe',
             'currency'=>'KES',
